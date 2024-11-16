728c414edfa95e42e2715fd15ad477068d35958b
Kyyberkass1 <54102850+Kyyberkass1@users.noreply.github.com>
2024-11-16 22:12:56 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+/*
+		Designed by: Guillaume Kurkdjian 
+		Original image: https://electronicitems.tumblr.com/page/2
+*/
+
+let b = document.body;
+let printer = document.querySelector("#printer");
+let shadows = document.querySelector("#shadows");
+let paper = document.querySelector("#paper");
+let paperShadow = document.querySelector("#paper-shadow");
+let text = document.querySelector("#text");
+let a = document.querySelector("#audio");
+
+
+
+/*******************/
+let printerFunction = () => {
+    printer.classList.toggle('is-printer-animated')
+    shadows.classList.toggle('is-shadows-animated')
+    paper.classList.toggle('is-paper-animated')
+    paperShadow.classList.toggle('is-paper-shadow-animated')
+    text.classList.toggle('is-text-animated')
+
+    a.loop = true;
+
+    if (a.paused) a.play();
+    else {
+        a.pause();
+        a.currentTime = 0;
+    }
+}
+
+/*******************/
+printer.addEventListener("click", printerFunction)
